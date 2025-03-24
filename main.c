20e4b597973bbc9ddc99cd785060a9db43842224
peethree <pieterschoptkont@gmail.com>
2025-03-24 13:18:08 +0100

    small journal cl app

--- main.c
+++ main.c
@@ -1,11 +1,38 @@
 #include <time.h>
+#include <stdio.h>
 
 #define NOB_IMPLEMENTATION
 #define NOB_STRIP_PREFIX
 #include "nob.h"
 
-// cc -o main main.c
-// ./main
+bool FileExists(char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file) {
+        fclose(file);
+        return true;  
+    }
+    // file doesn't exist
+    return false;  
+}
+
+void update_journal(int day, int month, int year, char entry[])
+{
+    // make sure file exists
+    if (!FileExists("journal.txt")) {
+        printf("Journal doesn't exist.\n");
+        printf("Creating new journal file...\n");
+        FILE *file = fopen("journal.txt", "w");
+        fprintf(file, "%02d/%02d/%04d\n", day, month, year);
+        fprintf(file, "%s\n", entry);
+        fclose(file); 
+    } else {
+        FILE *file = fopen("journal.txt", "a"); 
+        fprintf(file, "%02d/%02d/%04d\n", day, month, year);
+        fprintf(file, "%s\n", entry);
+        fclose(file);  
+    }
+}
+
 int main(int argc, char **argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
     Cmd cmd = {0};
@@ -23,10 +50,30 @@ int main(int argc, char **argv) {
     int day = tm_info->tm_mday + 1;
     int month = tm_info->tm_mon + 1;    
     int year = tm_info->tm_year + 1900;
-    
+
     printf("day: %d, month: %d, year: %d\n", day, month, year);
 
-    return 0;      
+    // buffer size
+    char todays_entry[1000];
+    printf("Write today's journal entry\n");   
+    // get user input 
+    fgets(todays_entry, sizeof(todays_entry), stdin);  
+
+    char answer[sizeof(char*)];
+    printf("Add today's entry to your journal? y/n\n");
+    scanf("%s", answer);
+    char *yes_answer = "y";
+
+    // if answer is yes, update journal
+    if (strcmp(answer, yes_answer) == 0) {
+        update_journal(day, month, year, todays_entry);   
+        printf("Adding entry to your journal\n");     
+    } else {
+        printf("Exiting journal\n");
+        return 1;
+    }        
+    printf("Exiting journal\n");
+    return 0;       
 }
 
 
