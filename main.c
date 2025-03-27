187f438af2f82e8bdd126b2045b3984fe92f5cf4
peethree <pieterschoptkont@gmail.com>
2025-03-27 16:13:47 +0100

    added a database to the journal, fixed some bugs as well as refactored -- unable to use nob.h with multiple source files, me big noop

--- main.c
+++ main.c
@@ -1,66 +1,123 @@
+// cc -o journal main.c sqlite3.c -lsqlite3 -Wall -Wextra -Wno-cast-function-type -Wno-unused-variable -Wno-implicit-fallthrough -Wno-unused-but-set-variable
 #include <time.h>
 #include <stdio.h>
+#include "sqlite3.h"
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 
-#define NOB_IMPLEMENTATION
-#define NOB_STRIP_PREFIX
-#include "nob.h"
+// #define NOB_IMPLEMENTATION
+// #define NOB_STRIP_PREFIX
+// #include "nob.h"
 
-bool FileExists(char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (file) {
-        fclose(file);
-        return true;  
-    }
-    // file doesn't exist
-    return false;  
-}
-
-void update_journal(int day, int month, int year, char entry[])
+void update_journal(int day, int month, int year, char entry[]) 
 {
-    // make sure file exists, make it otherwise
-    if (!FileExists("journal.txt")) {
-        printf("Journal doesn't exist.\n");
-        printf("Creating new journal file...\n");
-
-        FILE *file = fopen("journal.txt", "w");
-        fprintf(file, "%02d/%02d/%04d\n", day, month, year);
-        fprintf(file, "%s\n", entry);
-        fclose(file); 
-    } else {
-        FILE *file = fopen("journal.txt", "a"); 
-        fprintf(file, "%02d/%02d/%04d\n", day, month, year);
-        fprintf(file, "%s\n", entry);
-        fclose(file);  
+    // open file in append mode, create it if it doesn't exist
+    FILE *file = fopen("journal.txt", "a");
+    
+    if (file == NULL) {
+        printf("Error opening journal file\n");
+        return;
     }
+    
+    // write date and entry to the file
+    fprintf(file, "%02d/%02d/%04d\n%s\n", day, month, year, entry);    
+    fclose(file);
 }
 
-int main(int argc, char **argv) {
-    NOB_GO_REBUILD_URSELF(argc, argv);
-    Cmd cmd = {0};
-    cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", "journal", "main.c");
-    if (!nob_cmd_run_sync(cmd)) return 1;
+int insert_into_db(sqlite3 *db, char* todays_entry)
+{
+    // sql statement
+    sqlite3_stmt *statement;
+    char *sql = "insert into journal (text) values (?)";
+
+    int rc = sqlite3_prepare_v2(db, sql, -1, &statement, 0);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+        return rc;
+    }
+
+    // bind date to placeholder (1)
+    sqlite3_bind_text(statement, 1, todays_entry, -1, SQLITE_STATIC);
+
+    // evaluate statement
+    rc = sqlite3_step(statement);
+    if (rc != SQLITE_DONE) {
+        fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
+    }
+
+    // release memorty once finalized
+    sqlite3_finalize(statement);
+
+    return rc
+}
+
+void cleanup(char* todays_entry, sqlite3 *db) 
+{
+    free(todays_entry);
+    sqlite3_close(db);
+    printf("Exiting journal...\n");
+    printf("Closing database...\n");
+}
+
+int main() {   
+    // nob building script (doesn't seem to work with multiple source files) but it was quite convenient for only 1
+
+    // NOB_GO_REBUILD_URSELF(argc, argv);
+    // Cmd cmd = {0};
+    // cmd_append(&cmd, "cc", 
+    //     "-Wall", 
+    //     "-Wextra", 
+    //     "-Wno-cast-function-type", 
+    //     "-Wno-unused-variable", 
+    //     "-Wno-implicit-fallthrough", 
+    //     "-o", "journal", 
+    //     "main.c", 
+    //     "sqlite3.c", 
+    //     "-lsqlite3");
+
+    // if (!cmd_run_sync(cmd)) {
+    //     nob_log(NOB_ERROR, "Build failed");
+    //     return 1;
+    // }    
+    
+    // db
+    sqlite3 *db;
+    const char* db_name = "test.db";
+    int rc = sqlite3_open(db_name, &db);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    }
+    printf("Opened db successfully...\n");    
 
     // get time
     time_t t;
     struct tm *tm_info;
-
     time(&t);
     tm_info = localtime(&t);
   
-    // day/ month initialized at 0 so + 1, year at 1900
-    int day = tm_info->tm_mday + 1;
+    // month initialized at 0 so + 1, year at -1900
+    int day = tm_info->tm_mday;
     int month = tm_info->tm_mon + 1;    
     int year = tm_info->tm_year + 1900;
 
-    // printf("day: %d, month: %d, year: %d\n", day, month, year);
+    // dynamic buffer for user input
+    char *todays_entry = NULL;
+    size_t buffer_size = 0;
+    printf("Write today's journal entry:\n");   
+    size_t characters_read = getline(&todays_entry, &buffer_size, stdin);
+    printf("%zu\n", characters_read);
+ 
+    if (characters_read <= 1) {
+        printf("No input given...\n"); 
+        cleanup(todays_entry, db);        
+        return 1;
+    }
 
-    // buffer size
-    char todays_entry[1000];
-    printf("Write today's journal entry\n");   
-    // get user input 
-    fgets(todays_entry, sizeof(todays_entry), stdin);  
-
-    char answer[sizeof(char*)];
+    // buffer for 1 char + null terminator
+    char answer[sizeof(char) + 1];
     printf("Add today's entry to your journal? y/n\n");
     scanf("%s", answer);
     char *yes_answer = "y";
@@ -68,11 +125,13 @@ int main(int argc, char **argv) {
     // if answer is yes, update journal
     if (strcmp(answer, yes_answer) == 0) {
         update_journal(day, month, year, todays_entry);   
-        printf("Adding entry to your journal\n");     
-    } else {
-        printf("Exiting journal\n");
-        return 1;
-    }        
-    printf("Exiting journal\n");
+        printf("Adding entry to your journal\n"); 
+    }
+
+    // add it to database as well
+    insert_into_db(db, todays_entry);    
+        
+    cleanup(todays_entry, db);     
     return 0;       
 }
+
