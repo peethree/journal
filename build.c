12999795d95d68b61be0fcaae2b3cb3983c855e7
peethree <pieterschoptkont@gmail.com>
2025-03-29 16:42:51 +0100

    added raylib for reading db entries, default window for now

--- build.c
+++ build.c
@@ -3,7 +3,7 @@
 #include "nob.h"
 
 // cc -o build build.c
-// avoids recompiling sqlite3 every time, instead uses static object file
+
 int main(int argc, char** argv) {   
     // NOB_GO_REBUILD_URSELF(argc, argv);
     Cmd cmd = {0};
@@ -12,8 +12,18 @@ int main(int argc, char** argv) {
         "-Wextra", 
         "-o", "journal", 
         "./src/main.c", 
-        "./src/sqlite3.o",   
-        "-lsqlite3");
+        "./src/sqlite3.o",          // avoids recompiling sqlite3 every time from sqlite3.c, instead uses static object file
+        "-lsqlite3",                // sql
+        "./resource_dir.h",         // raylib
+        "-I/usr/local/include",         
+        "-L/usr/local/lib",         
+        "-lraylib",                 
+        "-lGL",                     
+        "-lm",                      
+        "-lpthread",                
+        "-ldl",                     
+        "-lrt"                      
+    );
 
     if (!cmd_run_sync(cmd)) {
         nob_log(NOB_ERROR, "Build failed");
